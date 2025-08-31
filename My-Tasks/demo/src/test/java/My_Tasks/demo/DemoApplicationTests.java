00e2e6ad804cb622dd6bf099022d47fac334860f
Leandro Martini <martinileandro12@gmail.com>
2025-08-30 21:34:14 -0300

    estrutura API para fazer o primeiro projeto FullStack

---
+++ My-Tasks/demo/src/test/java/My_Tasks/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package My_Tasks.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
