5a447d8eba6949cd88425ac19345e9c7631cec95
Leandro Martini <martinileandro12@gmail.com>
2025-09-25 18:04:54 -0300

    base da classe dos usuários

---
+++ Merx-Commerce/demo/src/main/java/com/merx_commerce/demo/entities/Enums/Roles.java
@@ -0,0 +1,6 @@
+package com.merx_commerce.demo.entities.Enums;
+
+public enum Roles {
+    USER,
+    ADMIN
+}
---
+++ Merx-Commerce/demo/src/main/java/com/merx_commerce/demo/entities/User.java
@@ -0,0 +1,44 @@
+package com.merx_commerce.demo.entities;
+
+import com.merx_commerce.demo.entities.Enums.Roles;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = " User name cannot be blank.")
+    private String name;
+
+    @CPF(message = "Invalid CPF")
+    private String cpf;
+
+    @Email(message = "Invalid Email")
+    private String email;
+
+    @NotBlank(message = " User password cannot be blank.")
+    private String password;
+
+    private Roles role = Roles.USER;
+
+    private List<Address> addresses = new ArrayList<>();
+}
