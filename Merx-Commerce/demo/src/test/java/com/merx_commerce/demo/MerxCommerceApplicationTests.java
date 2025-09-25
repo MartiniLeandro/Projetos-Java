775777c13eecf682bc0f5ebe71af4ffe7cbdc315
Leandro Martini <martinileandro12@gmail.com>
2025-09-25 10:14:36 -0300

    estrutura inicial projeto e-commerce

---
+++ Merx-Commerce/demo/src/test/java/com/merx_commerce/demo/MerxCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merx_commerce.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerxCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
