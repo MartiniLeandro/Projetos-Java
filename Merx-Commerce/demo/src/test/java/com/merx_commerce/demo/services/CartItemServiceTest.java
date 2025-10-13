626ad45b00c28e83ac12742fa7fba0b91ac66af2
Leandro Martini <martinileandro12@gmail.com>
2025-10-13 20:27:47 -0300

    Testando o edição de um item de carrinho para dentro de um carrinho

--- Merx-Commerce/demo/src/test/java/com/merx_commerce/demo/services/CartItemServiceTest.java
+++ Merx-Commerce/demo/src/test/java/com/merx_commerce/demo/services/CartItemServiceTest.java
@@ -1,11 +1,9 @@
 package com.merx_commerce.demo.services;
 
-import com.merx_commerce.demo.entities.Cart;
-import com.merx_commerce.demo.entities.Category;
+import com.merx_commerce.demo.entities.*;
 import com.merx_commerce.demo.entities.DTOS.*;
-import com.merx_commerce.demo.entities.Product;
-import com.merx_commerce.demo.entities.User;
 import com.merx_commerce.demo.exceptions.OutOfStockException;
+import com.merx_commerce.demo.repositories.CartItemRepository;
 import com.merx_commerce.demo.repositories.CartRepository;
 import com.merx_commerce.demo.repositories.UserRepository;
 import org.junit.jupiter.api.Assertions;
@@ -18,10 +16,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CartItemServiceTest {
@@ -34,6 +33,9 @@ public class CartItemServiceTest {
 
     @Mock
     private CartRepository cartRepository;
+
+    @Mock
+    private CartItemRepository cartItemRepository;
 
     @InjectMocks
     private CartItemService cartItemService;
@@ -50,7 +52,7 @@ public class CartItemServiceTest {
                 .name("keyboard")
                 .description("a magic keyboard")
                 .price(new BigDecimal(250))
-                .quantityInStock(0)
+                .quantityInStock(10)
                 .images(new ArrayList<>())
                 .category(new Category(1L,"test", null))
                 .build();
@@ -90,8 +92,24 @@ public class CartItemServiceTest {
         when(userService.findUserByToken(anyString())).thenReturn(userDTO);
         when(userRepository.findUserByEmail(userDTO.email())).thenReturn(u1);
         CartItemRequestDTO cartItemRequestDTO = new CartItemRequestDTO(1L,p1,u1.getCart(),1,new BigDecimal(250));
-        OutOfStockException exception = Assertions.assertThrows(OutOfStockException.class, () -> cartItemService.createCartItem(cartItemRequestDTO,"fake-token"));
+        OutOfStockException exception = Assertions.assertThrows(OutOfStockException.class, () -> cartItemService.createCartItem(cartItemRequestDTO,"fake-token")); //trocar quantidade no estoque dentro do BeforeEach
 
         Assertions.assertEquals("This product is out of stock",exception.getMessage());
+    }
+
+    @Test
+    void testUpdateCartItem(){
+        UserResponseDTO userDTO = new UserResponseDTO(u1);
+        when(userService.findUserByToken(anyString())).thenReturn(userDTO);
+        when(userRepository.findUserByEmail(userDTO.email())).thenReturn(u1);
+        CartItem cartItem = new CartItem(1L,p1,u1.getCart(),1,new BigDecimal(250));
+        CartItemRequestDTO cartItemRequestDTO = new CartItemRequestDTO(2L,p1,u1.getCart(),2,new BigDecimal(500));
+        u1.getCart().setItems(List.of(cartItem));
+        when(cartItemRepository.findById(anyLong())).thenReturn(Optional.of(cartItem));
+        when(cartItemRepository.save(any(CartItem.class))).thenReturn(cartItem);
+        CartItemResponseDTO cartItemResponseDTO = cartItemService.updateCartItem(cartItemRequestDTO,1L,"fake-token");
+
+        Assertions.assertEquals(2, cartItemResponseDTO.quantity());
+        Assertions.assertEquals(u1.getCart(),cartItemResponseDTO.cart());
     }
 }
