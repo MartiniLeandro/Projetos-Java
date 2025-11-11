ff67723dbd51ead5034f6b9df0c6c5e355130867
Leandro Martini <martinileandro12@gmail.com>
2025-11-10 22:53:32 -0300

    token service

---
+++ Week-Flow/src/main/java/com/WeekFlow/security/TokenService.java
@@ -0,0 +1,41 @@
+package com.WeekFlow.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.WeekFlow.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+public class TokenService {
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(System.getenv("JWT_SECRET"));
+            return JWT.create()
+                    .withIssuer("admin")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(Instant.now().plusSeconds(7200))
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("erro");
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(System.getenv("JWT_SECRET"));
+            return JWT.require(algorithm)
+                    .withIssuer("admin")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch(JWTVerificationException e){
+            throw new RuntimeException("token inválido");
+        }
+    }
+
+}
