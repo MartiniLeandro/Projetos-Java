353c71faa0470ce434780fff3e8be2a42da4d972
Leandro Martini <martinileandro12@gmail.com>
2025-11-24 22:56:07 -0300

    inicio dos testes unitários habitService

--- Week-Flow/src/main/java/com/WeekFlow/entities/DTOS/HabitRequestDTO.java
+++ Week-Flow/src/main/java/com/WeekFlow/entities/DTOS/HabitRequestDTO.java
@@ -2,10 +2,16 @@ package com.WeekFlow.entities.DTOS;
 
 import com.WeekFlow.entities.ENUMS.Status;
 import com.WeekFlow.entities.ENUMS.Week;
+import com.WeekFlow.entities.Habit;
 import com.WeekFlow.entities.User;
 
 import java.time.LocalTime;
 import java.util.List;
 
-public record HabitRequestDTO(Long id, String name, String description, Status status, LocalTime initialTime, LocalTime finalTime, List<Week> daysWeek, Boolean recurrent) {
+public record HabitRequestDTO(Long id, String name, String description, LocalTime initialTime, LocalTime finalTime, List<Week> daysWeek, Boolean recurrent) {
+    public HabitRequestDTO(Habit habit){
+        this(
+                habit.getId(), habit.getName(), habit.getDescription(), habit.getInitialTime(), habit.getFinalTime(), habit.getDaysWeek(), habit.getRecurrent()
+        );
+    }
 }
--- Week-Flow/src/test/java/com/WeekFlow/services/HabitServiceTest.java
+++ Week-Flow/src/test/java/com/WeekFlow/services/HabitServiceTest.java
@@ -1,5 +1,6 @@
 package com.WeekFlow.services;
 
+import com.WeekFlow.entities.DTOS.HabitRequestDTO;
 import com.WeekFlow.entities.DTOS.HabitResponseDTO;
 import com.WeekFlow.entities.ENUMS.Week;
 import com.WeekFlow.entities.Habit;
@@ -15,8 +16,9 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,5 +52,50 @@ public class HabitServiceTest {
 
         Assertions.assertEquals(2,allHabits.size());
         Assertions.assertEquals("gym",allHabits.getFirst().name());
+    }
+
+    @Test
+    void testGetHabitById(){
+        h1.setId(1L);
+        u1.setHabits(List.of(h1));
+        when(userService.findUserByToken(anyString())).thenReturn(u1);
+        when(habitRepository.findById(anyLong())).thenReturn(Optional.ofNullable(h1));
+        HabitResponseDTO habit = habitService.getHabitById("fake-token",1L);
+
+        Assertions.assertNotNull(habit);
+        Assertions.assertEquals("gym",habit.name());
+    }
+
+    @Test
+    void testCreateHabit(){
+        HabitRequestDTO newHabit = new HabitRequestDTO(h1);
+        when(userService.findUserByToken(anyString())).thenReturn(u1);
+        when(habitRepository.save(any(Habit.class))).thenReturn(h1);
+        HabitResponseDTO habit = habitService.createHabit("fake-token", newHabit);
+
+        Assertions.assertNotNull(habit);
+        Assertions.assertEquals("gym",habit.name());
+
+    }
+
+    @Test
+    void testUpdateHabit(){
+        h1.setId(1L);
+        HabitRequestDTO habitRequestDTO = new HabitRequestDTO(h2);
+        when(userService.findUserByToken(anyString())).thenReturn(u1);
+        when(habitRepository.findById(anyLong())).thenReturn(Optional.ofNullable(h1));
+        when(habitRepository.save(any(Habit.class))).thenReturn(h1);
+        HabitResponseDTO habit = habitService.updateHabit("fake-token", habitRequestDTO, 1L);
+
+        Assertions.assertNotNull(habit);
+        Assertions.assertEquals("gym",habit.name());
+    }
+
+    @Test
+    void testDeleteHabit(){
+        when(habitRepository.existsById(anyLong())).thenReturn(true);
+        habitService.deleteHabit(1L);
+
+        Mockito.verify(habitRepository).deleteById(1L);
     }
 }
