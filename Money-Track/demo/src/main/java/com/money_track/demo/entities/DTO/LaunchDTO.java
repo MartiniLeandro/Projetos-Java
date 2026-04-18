24b06ed7ffdaa8802db90032fbd9df0234935160
Leandro Martini <martinileandro12@gmail.com>
2026-04-18 13:39:37 -0300

    Ajustando os DTOs para Records

--- Money-Track/demo/src/main/java/com/money_track/demo/entities/DTO/LaunchDTO.java
+++ Money-Track/demo/src/main/java/com/money_track/demo/entities/DTO/LaunchDTO.java
@@ -5,54 +5,11 @@ import com.money_track.demo.entities.Launch;
 
 import java.time.LocalDate;
 
-public class LaunchDTO {
-    private final Long id;
-    private String description;
-    private Category category;
-    private Double value;
-    private LocalDate date;
+public record LaunchDTO(Long id, String description, Category category, Double value, LocalDate date){
 
-    public LaunchDTO(Launch launch){
-        this.id = launch.getId();
-        this.description = launch.getDescription();
-        this.category = launch.getCategory();
-        this.value = launch.getValue();
-        this.date = launch.getDate();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public Double getValue() {
-        return value;
-    }
-
-    public void setValue(Double value) {
-        this.value = value;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public LaunchDTO(Launch launch) {
+        this(
+                launch.getId(), launch.getDescription(), launch.getCategory(), launch.getValue(), launch.getDate()
+        );
     }
 }
