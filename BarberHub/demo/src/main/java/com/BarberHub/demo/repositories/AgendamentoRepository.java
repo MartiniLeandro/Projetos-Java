e8ec24c5bd791e7d6423e8106ce772995e2f11c6
Leandro Martini <martinileandro12@gmail.com>
2026-07-21 20:46:32 -0300

    começando a fazer os métodos para a tela de dashboard da barbearia

--- BarberHub/demo/src/main/java/com/BarberHub/demo/repositories/AgendamentoRepository.java
+++ BarberHub/demo/src/main/java/com/BarberHub/demo/repositories/AgendamentoRepository.java
@@ -4,8 +4,12 @@ import com.BarberHub.demo.entities.Agendamento;
 import com.BarberHub.demo.entities.Barbearia;
 import com.BarberHub.demo.entities.Barbeiro;
 import com.BarberHub.demo.entities.Cliente;
+import com.BarberHub.demo.entities.ENUMS.StatusCorte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,4 +20,10 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento,Long> {
     List<Agendamento> findAllByCliente(Cliente cliente);
     List<Agendamento> findByBarbeariaIdAndHoraInicialBetween(Long barbeariaId, LocalDateTime inicio, LocalDateTime fim);
     List<Agendamento> findByBarbeiroIdAndHoraInicialBetween(Long idBarbeiro, LocalDateTime inicio, LocalDateTime fim);
+    long countByBarbeariaIdAndHoraInicialBetween(Long barbeariaId, LocalDateTime startDay, LocalDateTime endDay);
+    long countByBarbeariaIdAndStatusCorteAndHoraInicialBetween(Long barbeariaId, StatusCorte statusCorte, LocalDateTime startDay, LocalDateTime endDay);
+
+    @Query(value = "select sum(ser.preco) from agendamentos as age inner join servicos as ser on age.servico_id = ser.id where age.barbearia_id = :barbeariaId and age.status = 'CONCLUIDO' and age.hora_inicial between :startDay and :endDay",nativeQuery = true)
+    Double getReceitaOfTheDay(@Param("barbeariaId") Long barbeariaId, @Param("startDay") LocalDateTime startDay, @Param("endDay") LocalDateTime endDay);
+
 }
---
+++ BarberHub/demo/src/main/java/com/BarberHub/demo/services/dashboard/DashboardBarbeariaService.java
@@ -0,0 +1,44 @@
+package com.BarberHub.demo.services.dashboard;
+
+import com.BarberHub.demo.entities.ENUMS.RoleUser;
+import com.BarberHub.demo.entities.ENUMS.StatusCorte;
+import com.BarberHub.demo.entities.User;
+import com.BarberHub.demo.exceptions.InvalidRoleException;
+import com.BarberHub.demo.repositories.AgendamentoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Service
+public class DashboardBarbeariaService {
+
+    private final AgendamentoRepository agendamentoRepository;
+
+    public DashboardBarbeariaService(AgendamentoRepository agendamentoRepository) {
+        this.agendamentoRepository = agendamentoRepository;
+    }
+
+    public long getAgendamentosQuantityByDate(LocalDate date, User user){
+        if(user.getRole() != RoleUser.BARBEARIA || user.getBarbearia() == null) throw new InvalidRoleException("Você não pode realizar esta ação");
+        LocalDateTime startDay = date.atStartOfDay();
+        LocalDateTime endDay = date.atTime(LocalTime.MAX);
+        return agendamentoRepository.countByBarbeariaIdAndHoraInicialBetween(user.getBarbearia().getId() ,startDay,endDay);
+    }
+
+    public long getAgendamentosRealizadosQuantityByDate(LocalDate date, User user){
+        if(user.getRole() != RoleUser.BARBEARIA || user.getBarbearia() == null) throw new InvalidRoleException("Você não pode realizar esta ação");
+        LocalDateTime startDay = date.atStartOfDay();
+        LocalDateTime endDay = date.atTime(LocalTime.MAX);
+        return agendamentoRepository.countByBarbeariaIdAndStatusCorteAndHoraInicialBetween(user.getBarbearia().getId() ,StatusCorte.CONCLUIDO, startDay, endDay);
+    }
+
+    public Double getReceitaByDate(LocalDate date, User user){
+        if(user.getRole() != RoleUser.BARBEARIA || user.getBarbearia() == null) throw new InvalidRoleException("Você não pode realizar esta ação");
+        LocalDateTime startDay = date.atStartOfDay();
+        LocalDateTime endDay = date.atTime(LocalTime.MAX);
+        Double receita = agendamentoRepository.getReceitaOfTheDay(user.getBarbearia().getId(),startDay,endDay);
+        return receita != null ? receita : 0.0;
+    }
+}
