7fb056a59a8110272a833d0183beb36aa1b0f858
Leandro Martini <martinileandro12@gmail.com>
2026-09-10 11:11:03 -0300

    adicionando a URL do deploy do frontend no CORS

--- gmassessoria/demo/src/main/java/com/martinileandro/gmassessoria/authentication/security/SecurityConfig.java
+++ gmassessoria/demo/src/main/java/com/martinileandro/gmassessoria/authentication/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://gmasessoria-frontend-beta.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
